d72da84dc0656a7d22b27ed9adef8e5b58f11d50
khan-abdullah0416 <khan.abdullah0416@gmail.com>
2024-07-09 16:05:03 +0100

    Added onclick events and walls to Node class
    Includes various variables renamed

--- Node.js
+++ Node.js
@@ -1,20 +1,44 @@
+let isStartSet = false;
+let isFinishSet = false;
+
 export default class Node {
-    constructor(htmlElement) {
-        this.htmlElement = htmlElement;
+    constructor(element) {
+        this.element = element;
+        this.element.addEventListener('click', () => {
+            if (!isStartSet) {
+                this.setStart();
+                isStartSet = true;
+            } else if (!isFinishSet && !this.isStart) {
+                this.setFinish();
+                isFinishSet = true;
+            } else if (!(this.isStart || this.isFinish)) {
+                this.setWall();
+            }
+        });
         this.isVisited = false;
         this.isStart = false;
         this.isFinish = false;
+        this.isWall = false;
     }
 
     setStart() {
-        this.htmlElement.classList.add('grid-item-start');
+        this.element.classList.add('node-start');
         this.isStart = true;
         this.isFinish = false;
+        this.isWall = false;
     }
 
     setFinish() {
-        this.htmlElement.classList.add('grid-item-finish');
+        this.element.classList.add('node-finish');
         this.isStart = false;
-        this.isFinish = true;  
+        this.isFinish = true;
+        this.isWall = false;
+    }
+
+    setWall() {
+        this.element.classList.add('node-wall');
+        this.isStart = false;
+        this.isFinish = false;
+        this.isWall = true;
     }
 }
